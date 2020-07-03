8d3458c0c39a170858cef139fcfd73871ea32d54
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-03 11:11:53 +0700

    Initial Commit

---
+++ FinancialManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinancialManagement
+//
+//  Created by Bao Vu on 7/3/20.
+//  Copyright © 2020 Bao Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
